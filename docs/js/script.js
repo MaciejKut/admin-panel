29f59717b3ef42c92194a205948b7269c726c539
MaciejKut <maciej.kutzmann@gmail.com>
2019-01-01 22:46:23 +0100

    Build package sent to docs folder

---
+++ docs/js/script.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    var menuIcon = document.getElementById('menu');
+    var leftNav = document.getElementById('navigation');
+
+    menuIcon.addEventListener('click', function () {
+        console.log('sprawdzenie czy wchodzi ');
+        //  console.log(menuIcon);
+        toggleVisible();
+
+    });
+
+    function toggleVisible() {
+        leftNav.classList.toggle('show');
+    }
+
+    if (document.querySelector('#overlay')) {
+
+        function closeModal() {
+            document.getElementById('overlay').classList.remove('show')
+        }
+
+        document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+            btn.addEventListener('click', function (e) {
+                e.preventDefault()
+                closeModal()
+            })
+        })
+
+        document.querySelector('#overlay').addEventListener('click', function (e) {
+            if (e.target === this) {
+                closeModal()
+            }
+        })
+
+        document.addEventListener('keyup', function (e) {
+            if (e.keyCode === 27) {
+                closeModal()
+            }
+        })
+
+        function openModal(modal) {
+            document.querySelectorAll('#overlay > *').forEach(function (modal) {
+                modal.classList.remove('show')
+            })
+            document.querySelector('#overlay').classList.add('show')
+
+            document.querySelector('#myModal').classList.add('show')
+        }
+
+
+        if (document.getElementsByClassName('btnAddNewBanner')) {
+
+            var modalBanner = document.getElementsByClassName('btnAddNewBanner');
+            var myModal = document.querySelector('#myModal');
+
+            modalBanner[0].addEventListener('click', function () {
+                console.log('sprawdzenie czy wchodzi ');
+                document.querySelector('#overlay').classList.add('show');
+                openModal(myModal);
+                // console.log(myModal);
+                // myModal.classList.toggle('show');
+
+            });
+        }
+
+    }
+}
