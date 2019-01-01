d298a3b9c1b032ec5a747419d6a8c7d6a717a6f5
MaciejKut <maciej.kutzmann@gmail.com>
2019-01-01 15:30:12 +0100

    Add modals - to add banners - first button activate

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = function () {
 
     menuIcon.addEventListener('click', function () {
         console.log('sprawdzenie czy wchodzi ');
-        console.log(menuIcon);
+        //  console.log(menuIcon);
         toggleVisible();
 
     });
@@ -12,5 +12,50 @@ window.onload = function () {
     function toggleVisible() {
         leftNav.classList.toggle('show');
     }
+
+    function closeModal() {
+        document.getElementById('overlay').classList.remove('show')
+    }
+
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            e.preventDefault()
+            closeModal()
+        })
+    })
+
+    document.querySelector('#overlay').addEventListener('click', function (e) {
+        if (e.target === this) {
+            closeModal()
+        }
+    })
+
+    document.addEventListener('keyup', function (e) {
+        if (e.keyCode === 27) {
+            closeModal()
+        }
+    })
+
+    function openModal(modal) {
+        document.querySelectorAll('#overlay > *').forEach(function (modal) {
+            modal.classList.remove('show')
+        })
+        document.querySelector('#overlay').classList.add('show')
+
+        document.querySelector('#myModal').classList.add('show')
+    }
+
+    var modalBanner = document.getElementsByClassName('btnAddNewBanner');
+    var myModal = document.querySelector('#myModal');
+
+    modalBanner[0].addEventListener('click', function () {
+        console.log('sprawdzenie czy wchodzi ');
+        document.querySelector('#overlay').classList.add('show');
+        openModal(myModal);
+        // console.log(myModal);
+        // myModal.classList.toggle('show');
+
+    });
+
 
 }
